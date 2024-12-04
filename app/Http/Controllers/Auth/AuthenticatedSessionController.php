baebb229121c15e074d6856c1fb3527e636bb9eb
kingchobo <kingchobo2022@gmail.com>
2024-12-05 07:49:05 +0900

    2024.12.05 3rd Dashboard 만들기

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,6 +28,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        // dd($request->user()->role);
+
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
