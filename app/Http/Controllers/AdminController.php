b471579ed95a91ac1ae685cb0992c0dcf0fe6a7f
kingchobo <kingchobo2022@gmail.com>
2024-12-20 07:00:21 +0900

    2024.12.20 7th : html to blade

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,6 +9,6 @@ class AdminController extends Controller
 
     public function AdminDashboard(Request $request) 
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     }
 }
