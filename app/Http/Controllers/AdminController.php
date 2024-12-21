6e537e48ec5b7f0ac1c71a6aaa9a9462a9b9c33a
kingchobo <kingchobo2022@gmail.com>
2024-12-21 10:15:53 +0900

    9th : logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -10,5 +11,14 @@ class AdminController extends Controller
     public function AdminDashboard(Request $request) 
     {
         return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout(); // 사용자를 로그아웃시킴
+        $request->session()->invalidate(); // 현재 세션 무효화
+        $request->session()->regenerateToken(); // CSRF 토큰 새로 생성
+
+        return redirect('/login'); // 해당 경로로 보냄
     }
 }
