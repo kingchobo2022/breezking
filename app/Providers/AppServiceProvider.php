6a1b17bfd120e7af9a2e04a86aaf4967f6dd6828
kingchobo <kingchobo2022@gmail.com>
2025-02-02 15:16:07 +0900

    55th: smtp 설정 동적 반영

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\SMTP;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        $row = SMTP::find(1);
+        if ($row) {
+            Config::set('mail.default', $row->mail_mailer);
+            Config::set('mail.mailers.smtp.host', $row->mail_host);
+            Config::set('mail.mailers.smtp.port', $row->mail_port);
+            Config::set('mail.mailers.smtp.username', $row->mail_username);
+            Config::set('mail.mailers.smtp.password', $row->mail_password);
+            Config::set('mail.mailers.smtp.encryption', $row->mail_encryption);
+            Config::set('mail.from.address', $row->mail_from_address);
+            Config::set('app.name', $row->app_name);
+        }
+
     }
 }
